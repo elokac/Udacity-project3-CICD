75a68713588bce1897ca9ada8156f220ca272d77
elokac <elokachiejina@gmail.com>
2022-07-12 19:47:52 +0100

    update cloudfront

---
+++ .circleci/delete.sh
@@ -0,0 +1,4 @@
+ echo "Destroying environment: << parameters.workflow_id >> "
+            aws s3 rm s3://udapeople-d0e368e --recursive
+            aws cloudformation delete-stack --stack-name udapeople-backend-d0e368e
+            aws cloudformation delete-stack --stack-name udapeople-frontend-d0e368e
